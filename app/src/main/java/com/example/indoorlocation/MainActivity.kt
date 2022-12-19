bebf271fb199b14962d64d54e823a878014d355d
PC <admin@PCs-MacBook-Pro.local>
2022-12-19 22:01:22 +0700

    second commit

--- app/src/main/java/com/example/indoorlocation/MainActivity.kt
+++ app/src/main/java/com/example/indoorlocation/MainActivity.kt
@@ -7,6 +7,11 @@ import android.util.Log
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver
+import com.lemmingapex.trilateration.TrilaterationFunction
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer
+import java.lang.Math.log
+import java.lang.Math.pow
 import java.util.*
 
 
@@ -14,35 +19,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val pixelGrid = PixelGridView(this)
-        val parent = LinearLayout(applicationContext)
-        val wifi_scanner_btn = Button(applicationContext)
+        var pixelGrid = PixelGridView(this)
+        var parent = LinearLayout(applicationContext)
+        var wifi_scanner_btn = Button(applicationContext)
         wifi_scanner_btn.setText("Wifi Scanner")
-        Log.v(TAG, " wifi_scanner_btn.setText")
-        val x = arrayOf(0f, 2f, 0f)
-        val y = arrayOf(0f, 0f, 2f)
-        val rssi = arrayOf(arrayOf(40f, 50f, 44f), arrayOf(0f, 0f, 0f), arrayOf(0f, 0f, 0f))
-        val positions = arrayOf(
+
+        var rssi = arrayOf(arrayOf(57.0, 50.0, 44.0), arrayOf(0, 0, 0), arrayOf(0, 0, 0))
+        var positions = arrayOf(
             doubleArrayOf(0.0, 0.0),
             doubleArrayOf(2.0, 0.0),
             doubleArrayOf(0.0, 2.0),
         )
 
-        val distances = doubleArrayOf(0.0, 0.0, 0.0)
-        for (int i = 0; i<   distances.size; i++) {
-            element = 1.a0
-        }
 
-        val solver = NonLinearLeastSquaresSolver(
+        var distances = (rssi.get(0).map {
+           5 * pow(10.0, (it as Double - 21.0 - 20.0 * log(10.0)) / 35.0)
+        }).toDoubleArray()
+
+        var solver = NonLinearLeastSquaresSolver(
             TrilaterationFunction(positions, distances),
             LevenbergMarquardtOptimizer()
         )
-        val optimum: Optimum = solver.solve()
+        var optimum = solver.solve()
 
 // the answer
 
 // the answer
-        val centroid: DoubleArray = optimum.getPoint().toArray()
+        var centroid = optimum.point.toArray()
+
+
+
+
+
         wifi_scanner_btn.setOnClickListener {
             // Do something in response to button click
             val intent = Intent(this, WifiScanner::class.java)
